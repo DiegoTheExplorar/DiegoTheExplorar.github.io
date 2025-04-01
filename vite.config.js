076f09fe19bce2e69893df831239f186319e8058
DiegoTheExplorar <arvindnatarajan2002@gmail.com>
2025-04-02 01:46:06 +0800

    Config change

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/DiegoTheExplorar.github.io', // This is the correct value if your repo is not username.github.io
+  base: '/', // This is the correct value if your repo is not username.github.io
 })
